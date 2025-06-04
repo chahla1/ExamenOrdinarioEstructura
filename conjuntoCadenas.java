ac5152b747719ba10d8b727be257614b10c87ad6
chahla1 <cbouchot@myuax.com>
2025-06-04 11:35:56 +0200

    commit

--- conjuntoCadenas.java
+++ conjuntoCadenas.java
@@ -1,7 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+
 public class conjuntoCadenas {
     private String[] conjunto;
     private int contador;
+    private boolean contiene(String nuevoElemento) {
+        return false;
+    }
 
     //Este es el constructor de la clase
     public conjuntoCadenas(){
@@ -9,11 +14,34 @@ public class conjuntoCadenas {
         contador = 0;
     }
 
+    //Este es el constructor de la clase con tamaño maximo
     public conjuntoCadenas(int max){
-
+        conjunto = new String[max];
+        contador = 0;
     }
 
-    public void anade(String nuevoElemento){
-
+    //Este es el conjunto anade
+    public void anade(String nuevoElemento) {
+        if (!contiene(nuevoElemento)) {
+            if (contador < conjunto.length) {
+                conjunto[contador++] = nuevoElemento;
+            } else {
+                System.out.println("El conjunto esta lleno, no se puede anadir mas elementos");
+            }
+        }
     }
+
+    public int tamano(){
+        return contador;
+    }
+
+    public void muestraElementosOrdenados(){
+        String[] copia = Arrays.copyOf(conjunto, contador);
+        Arrays.sort(copia);
+    }
+
+
+
+
+
 }
