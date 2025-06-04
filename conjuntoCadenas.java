68e91fa8db222f9a95b81c2707a67986cf6ccad2
chahla1 <cbouchot@myuax.com>
2025-06-04 11:24:34 +0200

    commit

---
+++ conjuntoCadenas.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+public class conjuntoCadenas {
+    private String[] Conjunto;
+    private int Contador;
+
+    //Este es el constructor de la clase
+    public conjuntoCadenas(){
+
+
+
+
+
+    }
+
+    public conjuntoCadenas(int max){
+
+    }
+
+    public void anade(String nuevoElemento){
+
+    }
+}
